9357fa5a112c1d8a5ca3a0ffb7a4c7c99e5176b9
alexanderras84 <alexanderras@gmail.com>
2025-07-11 12:32:38 +0800

    Update dynDNSCron.sh

--- dynDNSCron.sh
+++ dynDNSCron.sh
@@ -7,16 +7,23 @@ retVal=$?
 
 if [ $retVal -eq 124 ]; then
   echo "[ERROR] [DynDNSCron] generateACL.sh timed out!"
+  exit 1
 elif [ $retVal -ne 0 ]; then
   echo "[ERROR] [DynDNSCron] generateACL.sh failed with exit code $retVal!"
+  exit 1
 else
   echo "[INFO] [DynDNSCron] ACL successfully regenerated."
 
   echo "[INFO] [DynDNSCron] Restarting sing-box to apply new ACL..."
-  # Kill any existing sing-box processes (non-forceful)
+
+  # Gracefully stop existing sing-box instances (send SIGTERM)
   pkill -f 'sing-box run'
 
-  # Restart sing-box in background
+  # Wait a moment for graceful shutdown
+  sleep 2
+
+  # Start sing-box in background as the miniproxy user (optional, if needed)
+  # If running as root in container, you might want: su-exec miniproxy sing-box ...
   sing-box run -c /etc/sing-box/config.json &
 
   echo "[INFO] [DynDNSCron] sing-box restarted."
