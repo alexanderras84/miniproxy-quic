01355446a029299da8596f206b6717229aaadf18
alexanderras84 <alexanderras@gmail.com>
2025-07-11 12:56:37 +0800

    Update dynDNSCron.sh

--- dynDNSCron.sh
+++ dynDNSCron.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
+
 echo "[INFO] [DynDNSCron] Starting dynamic DNS ACL refresh..."
 
-# Run the ACL generator with a timeout
+# Regenerate the client list and allowed IPs
 timeout 40s /bin/bash /generateACL.sh
 retVal=$?
 
 if [ $retVal -eq 124 ]; then
   echo "[ERROR] [DynDNSCron] generateACL.sh timed out!"
-  exit 1
 elif [ $retVal -ne 0 ]; then
   echo "[ERROR] [DynDNSCron] generateACL.sh failed with exit code $retVal!"
-  exit 1
-fi
-
-echo "[INFO] [DynDNSCron] ACL successfully regenerated."
-
-# Apply new firewall ACL rules
-if /etc/miniproxy/acl_firewall.sh; then
-  echo "[INFO] [DynDNSCron] Firewall ACL rules applied successfully."
 else
-  echo "[ERROR] [DynDNSCron] Failed to apply firewall ACL rules!"
-  exit 1
+  echo "[INFO] [DynDNSCron] ACL list successfully regenerated."
+
+  echo "[INFO] [DynDNSCron] Applying updated firewall rules..."
+  /bin/bash /acl_firewall.sh
 fi
