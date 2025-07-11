f357aad47cbc1bfd95194f7b854a1547b130d2c9
alexanderras84 <alexanderras@gmail.com>
2025-07-11 13:04:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash -e
 
-echo "[INFO] Generating ACL and applying firewall rules..."
-
-# Generate allowed client list
+echo "[INFO] Generating ACL..."
 set +e
 /bin/bash /generateACL.sh
 set -e
 
-# Apply firewall rules using that list
-/bin/bash /acl_firewall.sh
-
-# If dynamic DNS was used, start cron refresh loop
 if [ "$DYNDNS_CRON_ENABLED" = true ]; then
   echo "[INFO] DynDNS detected — enabling cron job"
   echo "$DYNDNS_CRON_SCHEDULE /bin/bash /dynDNSCron.sh" > /etc/miniproxy/dyndns.cron
