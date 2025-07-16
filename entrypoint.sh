deb5d5801cc39878615cfeecaac2593e18748e16
alexanderras84 <alexanderras@gmail.com>
2025-07-16 16:13:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,23 @@
-#!/bin/bash
+#!/bin/bash -e
+
+echo "[INFO] Generating ACL..."
+set +e
+/bin/bash /generateacl.sh
 set -e
 
-# Generate the ACL and rebuild the sing-box config
-/generateacl.sh
+if [ "$DYNDNS_CRON_ENABLED" = true ]; then
+  echo "[INFO] DynDNS Address in ALLOWED_CLIENTS detected => Enable cron job"
+  echo "$DYNDNS_CRON_SCHEDULE /bin/bash /dyndnscron.sh" > /etc/miniproxy/dyndns.cron
+  supercronic /etc/miniproxy/dyndns.cron &
+fi
 
-# Ensure correct permissions (optional, for Docker volume cases)
-chown -R miniproxy:miniproxy /etc/sing-box/
+echo "[INFO] Starting sing-box.."
+sing-box run -c /etc/sing-box/config.json &
+singbox_pid=$!
 
-# Start sing-box as non-root user
-exec su-exec miniproxy sing-box run -c /etc/sing-box/config.json
+sleep 5
+
+echo "==================================================================="
+echo "[INFO] Miniproxy QUIC started"
+echo "==================================================================="
+wait $singbox_pid
