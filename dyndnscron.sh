bf75a2f919e2863eac5412cbe3a24eac6f0efa8e
alexanderras84 <alexanderras@gmail.com>
2025-07-16 20:59:24 +0800

    Update dyndnscron.sh

--- dyndnscron.sh
+++ dyndnscron.sh
@@ -10,12 +10,4 @@ elif [ $retVal -ne 0 ]; then
   echo "[ERROR] [DynDNSCron] generateacl.sh failed with exit code $retVal!"
 else
   echo "[INFO] [DynDNSCron] ACL regenerated!"
-  # Reload sing-box to apply new config (find running process and send SIGHUP)
-  SINGBOX_PID=$(pgrep -xo sing-box)
-  if [ -n "$SINGBOX_PID" ]; then
-    kill -SIGHUP "$SINGBOX_PID"
-    echo "[INFO] [DynDNSCron] sing-box successfully reloaded"
-  else
-    echo "[WARNING] [DynDNSCron] sing-box process not found, cannot reload"
-  fi
 fi
