0f42d6fc9d4c70791e6879bea1df5cfee7bf560c
alexanderras84 <alexanderras@gmail.com>
2025-07-16 14:02:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,4 @@
 #!/bin/bash -e
 
-echo "[INFO] Generating ACL..."
-set +e
-/bin/bash /generateACL.sh
-set -e
-
-echo "[INFO] Applying initial firewall rules..."
-/bin/bash /acl_firewall.sh
-
-if [ "$DYNDNS_CRON_ENABLED" = true ]; then
-  echo "[INFO] DynDNS detected — enabling cron job"
-  echo "$DYNDNS_CRON_SCHEDULE /bin/bash /dynDNSCron.sh" > /etc/miniproxy/dyndns.cron
-  supercronic /etc/miniproxy/dyndns.cron &
-fi
-
 echo "[INFO] Starting sing-box..."
 exec sing-box run -c /etc/sing-box/config.json
